3d98b0d3c18b3144adb6e4c24b5b29db8fa29551
sbsx <sandeshshrestha02@gmail.com>
2019-07-05 15:12:59 -0700

    hw

--- src/hello_world.mk
+++ src/hello_world.mk
@@ -1,6 +1,6 @@
 CSRCS += hello_world.c
 
-DEPPATH += --dep-path $(LVGL_DIR)C:\Users\sande\44730B-2019-TT\src
-VPATH += :$(LVGL_DIR)/lv_examples/lv_tutorial/1_hello_world
+DEPPATH += --dep-path $(LVGL_DIR)\44730B-2019-TT\src
+VPATH += :$(LVGL_DIR)\44730B-2019-TT\src
 
-CFLAGS += "-I$(LVGL_DIR)/lv_examples/lv_tutorial/1_hello_world"
+CFLAGS += "-I$(LVGL_DIR)\44730B-2019-TT\src"
