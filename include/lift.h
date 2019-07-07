2a20b08285b01e4a3f8cd48fd249c309efb9dec0
sbsx <sandeshshrestha02@gmail.com>
2019-07-07 12:35:59 -0700

    lift cont

--- include/lift.h
+++ include/lift.h
@@ -4,4 +4,17 @@
 #include "main.h"
 #include "okapi/api.hpp"
 
+const int LEFTLIFT_MOTOR = 11;
+const int RIGHTLIFT_MOTOR = 1;
+
+class Arm {
+public:
+  ChassisControllerIntegrated lift = AsyncControllerFactory::create(LEFTLIFT_MOTOR,
+     RIGHTLIFT_MOTOR, AbstractMotor::gearset::green);
+  ControllerButton = presetButtons[3] = {
+      ControllerButton(ControllerDigital::down),
+      ControllerButton(ControllerDigital::right),
+      ControllerButton(ControllerDigital::up)
+  };
+};
 #endif
